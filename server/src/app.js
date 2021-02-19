f5a01c3788ba52f55359afa2897749d2a183b022
Xperiencing <patrickmalonework@gmail.com>
2021-02-19 21:24:57 +0000

    Express server and angular project, with foundation code for web application.

---
+++ server/src/app.js
@@ -0,0 +1,37 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const roomList = [];
+
+io.on("connection", socket => {
+    let previousId;
+
+    const safeJoin = currentId => {
+      socket.leave(previousId);
+      socket.join(currentId, () => console.log(`Socket ${socket.id} joined room ${currentId}`));
+      previousId = currentId;
+    }
+
+    socket.on("joinRoom", user => {
+      safeJoin(user.roomId);
+
+      let room = roomList.find(x => x.roomId == user.roomId);
+
+      if(room && !room.userList.includes(user.username)) {
+        room.userList.push(user.username)
+      } else {
+        roomList.push({ roomId: user.roomId, userList: [user.username] });
+      }
+
+      socket.to(user.roomId).emit("userList", roomList.find(x => x.roomId == user.roomId).userList);
+    });
+
+    socket.on("sliderChange", slider => {
+      socket.to(slider.roomId).emit("slider", slider);
+    });
+});
+
+http.listen(4444, () => {
+  console.log('Listening on port 4444');
+});
